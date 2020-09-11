55b291b342879b6c109f88032c059b4f37089190
zhumabek <jumabekzalkarov@gmail.com>
2020-09-11 19:40:22 +0600

    #server apollo server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 require("dotenv").config();
 
+import {ApolloServer} from "apollo-server-express";
 import {connectDatabase} from "./database";
 import express, { Application } from "express";
+import {resolvers, typeDefs} from "./graphql";
 
 const mount = async (app: Application) => {
     await connectDatabase();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: ({req, res}) => ({ req, res })
+    });
+
+    server.applyMiddleware({ app, path: "/api" });
     app.listen(process.env.PORT);
 
     console.log(`[app] : http://localhost:${process.env.PORT}`);
