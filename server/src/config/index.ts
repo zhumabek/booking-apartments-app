57e42df1299674a66368a5d8be07b3abbd07e014
zhumabek <jumabekzalkarov@gmail.com>
2020-09-11 20:02:55 +0600

    #server updated database config url and connect function

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -3,13 +3,13 @@ let dbUrl:string;
 
 switch (process.env.NODE_ENV) {
     case 'development':
-        dbUrl = "mongodb://localhost/app";
+        dbUrl = "mongodb://localhost/booking-apartments-app";
         break;
     case 'production':
         dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
         break;
     default:
-        dbUrl = 'mongodb://localhost/app';
+        dbUrl = 'mongodb://localhost/booking-apartments-app';
 }
 
 export const config = {
