29b95672e64671c30e607af4f44427bd22505194
zhumabek <jumabekzalkarov@gmail.com>
2020-09-11 22:32:52 +0600

    #server Apartment model schema

---
+++ server/src/models/Apartment.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+const ApartmentSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    numOfRooms: {
+        type: Number,
+        default: 0
+    },
+    image: {
+        type: String
+    },
+    imagePublicId: {
+        type: String
+    },
+    checkInDate: {
+        type: Date,
+        required: true
+    },
+    checkOutDate: {
+        type: Date,
+        required: true
+    }
+});
+
+export interface IApartment extends Document {
+    name: string;
+    description?: string;
+    price: number;
+    numOfRooms: number;
+    image?: string;
+    imagePublicId?: string;
+    checkInDate: Date;
+    checkOutDate: Date;
+}
+
+export default mongoose.model<IApartment>('Apartment', ApartmentSchema);
