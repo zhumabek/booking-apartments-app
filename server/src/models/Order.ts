a9bd8b7a2e3d55b0ce87f4c03b60f00b796435b2
zhumabek <jumabekzalkarov@gmail.com>
2020-09-12 15:26:12 +0600

    #server Order model schema

---
+++ server/src/models/Order.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from "mongoose";
+import {IUser} from "./User";
+import {IVoucher} from "./Voucher";
+
+const OrderSchema = new Schema({
+    voucherId: {
+        type: Schema.Types.ObjectId,
+        ref: "Voucher"
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+});
+
+export interface IOrder extends Document {
+    voucherId: IVoucher["_id"];
+    userId: IUser["_id"];
+}
+
+export default mongoose.model<IOrder>('Order', OrderSchema);
