1d7b621e4f6a6ceef7bd54ad362e073c753aa7cf
zhumabek <jumabekzalkarov@gmail.com>
2020-09-12 14:48:59 +0600

    #server ApartmentTimeSlot model schema

---
+++ server/src/models/ApartmentTimeSlot.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+import {IApartment} from "./Apartment";
+
+const ApartmentTimeSlotSchema = new Schema({
+    date: {
+        type: Date,
+        required: true,
+    },
+    isBooked: {
+        type: Boolean,
+        default: false
+    },
+    apartmentId: {
+        type: Schema.Types.ObjectId,
+        ref: "Apartment"
+    }
+});
+
+export interface IApartmentTimeSlot extends Document {
+    date: Date;
+    isBooked: boolean;
+    apartmentId: IApartment["_id"];
+}
+
+export default mongoose.model<IApartmentTimeSlot>('ApartmentTimeSlot', ApartmentTimeSlotSchema);
