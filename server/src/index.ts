966952bdf5416f1f2a56bead8af719ef20a735fe
zhumabek <jumabekzalkarov@gmail.com>
2020-09-12 22:06:05 +0600

    #server added cookie-parser to the app

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,9 +5,13 @@ import {ApolloServer} from "apollo-server-express";
 import {connectDatabase} from "./database";
 import express, { Application } from "express";
 import {resolvers, typeDefs} from "./graphql";
+import cookieParser from "cookie-parser";
+
 
 const mount = async (app: Application) => {
     await connectDatabase();
+    app.use(cookieParser(process.env.COOKIE_SECRET));
+
     const server = new ApolloServer({
         typeDefs,
         resolvers,
