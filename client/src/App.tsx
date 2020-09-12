d79cc80d9593516a626aaca64b8d99abe5d64636
zhumabek <jumabekzalkarov@gmail.com>
2020-09-12 23:43:46 +0600

    #client Home, Login, Notfound routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import "./styles/index.css";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {Home, NotFound, Login} from "./containers";
+
 
 function App() {
-    return <div>Hello</div>;
+    return (
+        <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={Login}/>
+
+                    <Route path="/*" component={NotFound} />
+                </Switch>
+        </BrowserRouter>
+    )
 }
 
 export default App;
