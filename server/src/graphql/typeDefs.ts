cdee5eceab2ef745cbbc76c31e1686c758bf9920
zhumabek <jumabekzalkarov@gmail.com>
2020-09-13 21:24:50 +0600

    #server typeDefs for create apartment mutation

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,6 +1,17 @@
 import { gql } from "apollo-server-express";
 
-export const typeDefs = gql`    
+export const typeDefs = gql`
+    
+    type Mutation {
+        signUp(input: SignUpInput): User!
+        signIn(input: SignInInput): User!
+        signOut: User!
+        apartment(input: ApartmentInput): Apartment!
+    }
+
+    type Query {
+        hello: String
+    }
     
     type User {
         _id: ID
@@ -23,14 +34,38 @@ export const typeDefs = gql`
         firstName: String!
         lastName: String!
     }
-    
-    type Mutation {
-        signUp(input: SignUpInput): User!
-        signIn(input: SignInInput): User!
-        signOut: User!
+
+    type Apartment {
+        _id: ID!
+        name: String!
+        description: String!
+        price: Int!
+        numOfRooms: Int!
+        image: String!
+        imagePublicId: String!
+        owner: ID!
+        timeSlots: [ApartMentTimeSlot!]!
     }
     
-    type Query {
-        hello: String
+    type ApartMentTimeSlot {
+        _id: ID!
+        date: String!
+        isBooked: Boolean!
+        apartmentId: ID!
     }
+    
+    input ApartmentTimeSlotInput {
+        date: String!
+        isBooked: Boolean!
+    }
+    
+    input ApartmentInput {
+        name: String!
+        description: String
+        price: Int!
+        numOfRooms: Int!
+        image: String
+        timeSlots: [ApartmentTimeSlotInput!]!
+    }
+    
 `;
