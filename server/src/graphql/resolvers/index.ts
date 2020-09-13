5838d3a4fb20d1faeb917afb05987b887695ff4a
zhumabek <jumabekzalkarov@gmail.com>
2020-09-13 21:28:18 +0600

    #server merging multiple resolvers by lodash.merge

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,5 +1,5 @@
+import merge from "lodash.merge"
 import { userResolvers } from "./User";
+import {apartmentResolvers} from "./Apartment";
 
-export const resolvers = {
-    ...userResolvers,
-};
+export const resolvers = merge(userResolvers, apartmentResolvers);
