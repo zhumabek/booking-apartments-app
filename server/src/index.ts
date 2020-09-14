1d3c7a8fd2ff683568f228d7030a9ac45d2aea22
zhumabek <jumabekzalkarov@gmail.com>
2020-09-14 23:33:14 +0600

    #server added body parser middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,12 @@ import {connectDatabase} from "./database";
 import express, { Application } from "express";
 import {resolvers, typeDefs} from "./graphql";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 
 const mount = async (app: Application) => {
     await connectDatabase();
+    app.use(bodyParser.json({ limit: "5mb" }));
     app.use(cookieParser(process.env.COOKIE_SECRET));
 
     const server = new ApolloServer({
